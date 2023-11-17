9b39bf24242e20f4737e8e0b5ba859296afb797b
andre.zicatti <andrezicatti@gmail.com>
2023-11-17 19:36:17 -0300

    .

---
+++ JAVASCRIPT/aula002 (Modo Restrito)/aula002.js
@@ -0,0 +1,6 @@
+"use strict" // MODO RESTRITO
+
+let nome = `André` // a variavel deve ser identificada
+console.log(`CFB Cursos`) // podemos escrever strings
+console.log(nome) // podemos chamar variáveis
+console.log(`Nome: ${nome}`) // e podemos concatenar as duas
