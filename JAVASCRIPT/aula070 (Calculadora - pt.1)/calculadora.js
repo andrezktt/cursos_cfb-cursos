4fcd6ec5dc8c438cca7bc0fc1a42ec6e10896eaa
andre.zicatti <andrezicatti@gmail.com>
2023-12-15 16:38:56 -0300

    15122023

---
+++ JAVASCRIPT/aula070 (Calculadora - pt.1)/calculadora.js
@@ -0,0 +1,14 @@
+const teclasNumeros = [...document.querySelectorAll(".num")]
+const teclasOperacoes = [...document.querySelectorAll(".oper")]
+const teclaResultado = document.querySelector(".res")
+const display = document.querySelector(".display")
+
+console.log(teclasNumeros)
+console.log(teclasOperacoes)
+console.log(teclaResultado)
+
+teclasOperacoes.map((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        display.innerHTML += evento.target.innerHTML
+    })
+})
