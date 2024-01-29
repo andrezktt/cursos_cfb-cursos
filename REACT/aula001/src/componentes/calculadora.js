72a5857f143335df2fbb842975e3d0ca29ad1b9f
andre_zktt <andrezicatti@gmail.com>
2024-01-29 10:42:56 -0300

    26012024

---
+++ REACT/aula001/src/componentes/calculadora.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+
+export default function Calculadora() {
+    const [valorTela, setValorTela] = useState('')
+    const [resultado, setResultado] = useState(0)
+    const [acumulador, setAcumulador] = useState(0)
+    const [operacao, setOperacao] = useState(false)
+
+    return (
+        <>
+        </>
+    )
+}
